6073bccb37fa702f6ddd258578d1ba922807354d
Szilárd Kósa <kszilard777@gmail.com>
2022-06-05 13:18:18 +0200

    added touchscreen support for the press

--- scripts/me.js
+++ scripts/me.js
@@ -2,11 +2,22 @@ const meImg1 = document.getElementById('me-img1')
 const meImg2 = document.getElementById('me-img2')
 const me = document.getElementById('me')
 
-me.addEventListener('mousedown', function () {
+me.addEventListener('mousedown', onPress)
+me.addEventListener('mouseup', onRelease)
+me.addEventListener('mouseleave', onRelease)
+
+me.addEventListener('touchstart', onPress)
+me.addEventListener('touchend', onRelease)
+me.addEventListener('touchcancel', onRelease)
+
+function onPress(event) {
   meImg1.src = 'assets/me1pressed.png'
   meImg2.src = 'assets/me2pressed.png'
-})
-me.addEventListener('mouseup', function () {
+  event.preventDefault()
+}
+
+function onRelease(event) {
   meImg1.src = 'assets/me1.png'
   meImg2.src = 'assets/me2.png'
-})
+  event.preventDefault()
+}
